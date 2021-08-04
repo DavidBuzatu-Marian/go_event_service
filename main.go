9bf50b58cb32034b3fac1914129dd22429aae7a3
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-08-04 19:25:34 +0100

    Updated to use rest end-point to get events

--- main.go
+++ main.go
@@ -1,31 +1,6 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"os"
-)
-
-type Config struct {
-	MongoURI string
-}
-
-var config Config
-
-func ReadConfig() {
-	file, _ := os.Open("./config/default.json")
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	config = Config{}
-	err := decoder.Decode(&config)
-	if err != nil {
-		fmt.Println("Error reading config file!")
-		os.Exit(1)
-	}
-}
-
 func main() {
-	ReadConfig()
-	// PrintEvents()
-	TestCalendar()
+	PrintEvents()
+	// TestCalendar()
 }
--- scheduler.go
+++ scheduler.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/DavidBuzatu-Marian/go_mongo"
+	"log"
+	"net/http"
+	"time"
 )
 
+type Event []struct {
+	ID         string    `json:"_id"`
+	Name       string    `json:"name"`
+	StartDate  time.Time `json:"start_date"`
+	EndDate    time.Time `json:"end_date"`
+	Location   string    `json:"location"`
+	Directions string    `json:"directions"`
+	Details    string    `json:"details"`
+	DateAdded  time.Time `json:"date_added"`
+}
+
+var httpClient = &http.Client{Timeout: 10 * time.Second}
+
 func PrintEvents() {
-	client := go_mongo.ConnectToMongo(config.MongoURI)
-	events := go_mongo.CollectEvents(client)
-	for _, val := range events {
+	events := new(Event)
+	err := getPastHourEvents("http://localhost:8080/api/event/info/latest", events)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, val := range *events {
 		fmt.Println(val)
 	}
+}
+
+func getPastHourEvents(url string, target interface{}) error {
+	response, err := httpClient.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	return json.NewDecoder(response.Body).Decode(&target)
 }
